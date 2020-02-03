39b5ab73f68faa324d15d8be67cb8aa8a1215f78
Natalia Goska <natalia.goska@agrometrika.com.br>
2020-02-03 16:40:31 -0300

    save pdf

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React,  {useRef } from "react";
 import Grid from "@material-ui/core/Grid";
 import { Curriculum } from "./index";
+import { savePDF } from '@progress/kendo-react-pdf';
+import Paper from "@material-ui/core/Paper";
+
 function Home(props) {
+  const bodyRef = useRef();
+
+  const createPdf = () => savePDF(bodyRef.current, { 
+    paperSize: 'Letter',
+    fileName: 'test.pdf',
+    margin: 0
+  })
+
   return (
     <Grid container className="app">
       <Grid item xs={12} md={4}>
-        form
+        <button onClick={createPdf}>Create PDF</button>
       </Grid>
-      <Grid item xs={12} md={8}>
-        <Curriculum />
+      <Grid item xs={12} md={8} >
+        <Paper elevation={5} paperSize="" style={{ height: "100%" }} ref={bodyRef}>
+          <Curriculum/>
+        </Paper>
       </Grid>
     </Grid>
   );
 }
 export default Home;
+
--- src/views/curriculum/Curriculum.js
+++ src/views/curriculum/Curriculum.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import Paper from "@material-ui/core/Paper";
-import BackgroundImage from "../../components/backgroundImage";
+
+import {BackgroundImage} from "../../components/index";
 import { Grid } from "@material-ui/core";
 import SchoolIcon from "@material-ui/icons/School";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
@@ -15,31 +15,29 @@ import { Section } from "../../components/index";
 function Curriculum(props) {
   const { experience, education, languages, skills } = props;
   return (
-    <Paper elevation={5} style={{ minHeight: 1000 }}>
-      <Grid container direction="column" spacing={0}>
-        <Grid item xs={12}>
-          <BackgroundImage>
-            <Header />
-          </BackgroundImage>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={12}>
-              <Section title="EDUCATION" listInfo={education} icon={<SchoolIcon />} />
-            </Grid>
-            <Grid item xs={12} >
-              <Section title="EXPERIENCE" listInfo={experience} icon={<BusinessCenterIcon />}/>
-            </Grid>
-            <Grid item xs={5}>
-              <Section title="LANGUAGES" listInfo={languages} icon={<LanguageIcon />} gridSize={6}/>
-            </Grid>
-            <Grid item xs={7}>
-              <Section title="SKILLS" listInfo={skills} icon={<CodeIcon />} />
-            </Grid>
+    <Grid container direction="column" >
+      <Grid item xs={12}>
+        <BackgroundImage>
+          <Header />
+        </BackgroundImage>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Section title="EDUCATION" listInfo={education} icon={<SchoolIcon />} />
+          </Grid>
+          <Grid item xs={12} >
+            <Section title="EXPERIENCE" listInfo={experience} icon={<BusinessCenterIcon />}/>
+          </Grid>
+          <Grid item xs={5}>
+            <Section title="LANGUAGES" listInfo={languages} icon={<LanguageIcon />} gridSize={6}/>
+          </Grid>
+          <Grid item xs={7}>
+            <Section title="SKILLS" listInfo={skills} icon={<CodeIcon />} />
           </Grid>
         </Grid>
       </Grid>
-    </Paper>
+    </Grid>
   );
 }
 
