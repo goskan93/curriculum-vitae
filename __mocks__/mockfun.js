285458882d347abcdc598602f45b1acd2bb6fe1e
Natalia Goska <natalia.goska@agrometrika.com.br>
2020-02-04 13:47:52 -0300

    mock

---
+++ __mocks__/mockfun.js
@@ -0,0 +1,4 @@
+// let __value = 42;
+// const mockTest = jest.fn(() => __value);
+// mockTest.__setValue = v => __value = v;
+// export default mockTest;
--- src/__tests__/section.test.js
+++ src/__tests__/section.test.js
@@ -1,20 +1,24 @@
 describe('The section', () => {
-    beforeEach(() => {
-        console.log('BeforeEach');
-    });
-    beforeAll(() => {
-        console.log('BeforeAll');
-    });
-    afterEach(() => {
-        console.log('AfterEach')
-    });
-    afterAll(() =>{
-        console.log('AfterAll')
-    });
-    it.only('just test jest', () => {
-        expect(2+2).toEqual(4);
-    });
-    it.skip('just test jest 2', () => {
-        expect(2+5).toEqual(7);
-    });
+  beforeEach(() => {
+    console.log('BeforeEach');
+  });
+  beforeAll(() => {
+    console.log('BeforeAll');
+  });
+  afterEach(() => {
+    console.log('AfterEach')
+  });
+  afterAll(() =>{
+    console.log('AfterAll')
+  });
+  it.only('just test jest', () => {
+    expect(2+2).toEqual(4);
+  });
+  it.skip('just test jest 2', () => {
+    expect(2+5).toEqual(7);
+  });
 });
+
+// describe('test mock', () => {
+
+// });
