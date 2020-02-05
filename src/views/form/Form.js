d4d74305d897bfc1ec40cc1e3b39bf9927d02ce0
Natalia Goska <natalia.goska@agrometrika.com.br>
2020-02-05 17:55:57 -0300

    fix onChange

--- src/views/form/Form.js
+++ src/views/form/Form.js
@@ -8,20 +8,19 @@ function Form(props) {
   const {form} = props
 
   const onChangePersonalInfo = fieldName => value => {
-    console.log(value)
-    // props.dispatch(updatePersonalInfo({field: fieldName, value: value }));
+    props.dispatch(updatePersonalInfo({field: fieldName, value: value }));
   };
   const onChangeSectionInfo = fieldName => value => {
   };
 
   return (
     <>
-      <TextInput label="Name" value={form.name} xs={6} onChange={value => onChangePersonalInfo('name', value)}/>
-      <TextInput label="Email" value={form.email} xs={6} onChange={value => onChangePersonalInfo('email', value)}/>
-      <TextInput label="Phone" value={form.phone} xs={6} onChange={value => onChangePersonalInfo('phone', value)}/>
-      <TextInput label="GitHub" value={form.github} xs={6} onChange={value => onChangePersonalInfo('github', value)}/>
-      <TextInput label="Linkedin" value={form.linkedin} xs={6} onChange={value => onChangePersonalInfo('linkedin', value)}/>
-      <TextInput label="Website" value={form.website} xs={6} onChange={value => onChangePersonalInfo('website', value)}/>
+      <TextInput label="Name" value={form.name} xs={6} onChange={onChangePersonalInfo('name')}/>
+      <TextInput label="Email" value={form.email} xs={6} onChange={onChangePersonalInfo('email')}/>
+      <TextInput label="Phone" value={form.phone} xs={6} onChange={onChangePersonalInfo('phone')}/>
+      <TextInput label="GitHub" value={form.github} xs={6} onChange={onChangePersonalInfo('github')}/>
+      <TextInput label="Linkedin" value={form.linkedin} xs={6} onChange={onChangePersonalInfo('linkedin')}/>
+      <TextInput label="Website" value={form.website} xs={6} onChange={onChangePersonalInfo('website')}/>
     </>
   );
 }
