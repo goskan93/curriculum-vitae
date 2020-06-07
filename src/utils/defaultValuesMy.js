eab643ea4637b53e421297a19e197f10b9e61e51
goskan93 <natalia.goska@gmail.com>
2020-06-07 14:53:52 -0300

    chenge my defaylt values

--- src/utils/defaultValuesMy.js
+++ src/utils/defaultValuesMy.js
@@ -3,111 +3,99 @@ export const email = "natalia.goska@gmail.com";
 export const phone = "+55 19 98450-6605";
 export const github = "https://github.com/goskan93";
 export const linkedin = "https://www.linkedin.com/in/natalia-goska/";
-export const website = "https://goskan93.github.io/curriculum-vitae/";
+export const website = "https://goskan93.github.io/portfolio_v1/";
 
 export const education = [
   {
-    primaryText:
-      "03.2016 - 07.2017: Master of Science degree, Mathematics for Industry and Commerce",
-    secondaryText: "Wroclaw University of Science and Technology (Poland) "
+    primaryText: "03.2016 - 07.2017: Master of Science degree, Mathematics for Industry and Commerce",
+    secondaryText: "Wroclaw University of Science and Technology (Poland) ",
   },
   {
-    primaryText:
-      "09.2016 - 01.2017: Erasmus+ programme exchange student, Applied Mathematics",
-    secondaryText: "University of Coimbra (Portugal)"
+    primaryText: "09.2016 - 01.2017: Erasmus+ programme exchange student, Applied Mathematics",
+    secondaryText: "University of Coimbra (Portugal)",
   },
   {
-    primaryText:
-      "10.2012 - 01.2016: Bachelor of Science degree, Applied Mathematics",
-    secondaryText: "Wroclaw University of Science and Technology (Poland)"
-  }
+    primaryText: "10.2012 - 01.2016: Bachelor of Science degree, Applied Mathematics",
+    secondaryText: "Wroclaw University of Science and Technology (Poland)",
+  },
 ];
 
 export const experience = [
   {
-    primaryText:
-      "09.2018 - present: System Developer, Agrometrika, Vinhedo, Brazil",
+    primaryText: "09.2018 - present: System Developer, Agrometrika, Vinhedo, Brazil",
     secondaryText: `I am developing new functionalities for the system for credit analysis for agrobusiness and
       preparing data visualizations for business analysis.
-      I created mobile aplication integrated with the system.`
+      I created mobile aplication integrated with the system.`,
   },
   {
-    primaryText:
-      "04.2017 - 12.2017: Junior SAP PO Consultant, AtosGDBC, Wroclaw, Poland",
-    secondaryText: `I was doing data migration with my team to other environment and preparing migration's documentation.`
+    primaryText: "04.2017 - 12.2017: Junior SAP PO Consultant, AtosGDBC, Wroclaw, Poland",
+    secondaryText: `I was doing data migration with my team to other environment and preparing migration's documentation.`,
   },
   {
-    primaryText:
-      "06.2013 - 12.2014: Customer Service/Project Coordinator, Manus Foundation, Wroclaw, Poland",
+    primaryText: "06.2013 - 12.2014: Customer Service/Project Coordinator, Manus Foundation, Wroclaw, Poland",
     secondaryText: `I was helping during the students projects such as Student Camp, action of selling insurances for students, fam trip for students.
-      I was responsible for contact with participants, settling the formal issues and controlling the budget.`
-  }
+      I was responsible for contact with participants, settling the formal issues and controlling the budget.`,
+  },
 ];
 
 export const languages = [
   {
     primaryText: "English",
-    secondaryText: "advance"
+    secondaryText: "advance",
   },
   {
     primaryText: "Polish",
-    secondaryText: "native"
+    secondaryText: "native",
   },
   {
     primaryText: "Portuguese",
-    secondaryText: "intermediate"
+    secondaryText: "intermediate",
   },
   {
     primaryText: "German",
-    secondaryText: "basic"
-  }
+    secondaryText: "basic",
+  },
 ];
 
 export const skills = [
   {
-    primaryText: "Web Developmnet: ASP.NET MVC, C#, HTML, CSS, JS, ReactJS"
+    primaryText: "Web Developmnet: ASP.NET MVC, C#, HTML, CSS, JavaScript, ReactJS",
   },
   {
-    primaryText: "Data Analytics: Python, Power BI, MSSQL, MS Excel"
+    primaryText: "Data Analytics: Python, Power BI, MSSQL, MS Excel",
   },
   {
-    primaryText: "Mobile Development: React Native, Redux, Expo"
+    primaryText: "Mobile Development: React Native, Redux, Expo",
   },
-  {
-    primaryText: "Tools: GIT, Azure DevOps"
-  }
 ];
 
 export const others = [
   {
-    primaryText:
-      "10.2015 - 06.2016: President of Scientific Circle of Financial Engineering (volunteer)"
+    primaryText: "10.2015 - 06.2016: President of Scientific Circle of Financial Engineering (volunteer)",
+  },
+  {
+    primaryText: "10.2015 - 06.2016: Main Coordinator of Nationwide Mathematical Conference OMatKo! (volunteer)",
+  },
+  {
+    primaryText: "Rector’s scholarship in regard to high academic results (academic years 2012/2013 and 2015/2016)",
   },
   {
     primaryText:
-      "10.2015 - 06.2016: Main Coordinator of Nationwide Mathematical Conference OMatKo! (volunteer)"
+      "Distinction from the Rector and the Dean of the Faculty of Mathematics in regard to the exceptional social and student’s activity (academic year 2016/2017)",
   },
-  {
-    primaryText:
-      "Rector’s scholarship in regard to high academic results (academic years 2012/2013 and 2015/2016)"
-  },
-  {
-    primaryText:
-      "Distinction from the Rector and the Dean of the Faculty of Mathematics in regard to the exceptional social and student’s activity (academic year 2016/2017)"
-  }
 ];
 
 export const hobby = [
   {
-    primaryText: "travelling, learning new languages"
+    primaryText: "travelling, learning new languages",
   },
   {
-    primaryText: "volunteering on musical concerts/festivals"
+    primaryText: "volunteering on musical concerts/festivals",
   },
   {
-    primaryText: "reading books, running Instagram profile @_booksforchange "
+    primaryText: "reading books, running Instagram profile @_booksforchange ",
   },
   {
-    primaryText: "dancing"
-  }
+    primaryText: "dancing",
+  },
 ];
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,28 @@
-import { languages } from "./defaultValues";
-import { education } from "./defaultValues";
-import { experience } from "./defaultValues";
-import { name } from "./defaultValues";
-import { phone } from "./defaultValues";
-import { email } from "./defaultValues";
-import { skills } from "./defaultValues";
-import { github } from "./defaultValues";
-import { linkedin } from "./defaultValues";
-import { others } from "./defaultValues";
-import { website } from "./defaultValues";
-import { hobby } from "./defaultValues";
+// import { languages } from "./defaultValues";
+// import { education } from "./defaultValues";
+// import { experience } from "./defaultValues";
+// import { name } from "./defaultValues";
+// import { phone } from "./defaultValues";
+// import { email } from "./defaultValues";
+// import { skills } from "./defaultValues";
+// import { github } from "./defaultValues";
+// import { linkedin } from "./defaultValues";
+// import { others } from "./defaultValues";
+// import { website } from "./defaultValues";
+// import { hobby } from "./defaultValues";
+
+import { languages } from "./defaultValuesMy";
+import { education } from "./defaultValuesMy";
+import { experience } from "./defaultValuesMy";
+import { name } from "./defaultValuesMy";
+import { phone } from "./defaultValuesMy";
+import { email } from "./defaultValuesMy";
+import { skills } from "./defaultValuesMy";
+import { github } from "./defaultValuesMy";
+import { linkedin } from "./defaultValuesMy";
+import { others } from "./defaultValuesMy";
+import { website } from "./defaultValuesMy";
+import { hobby } from "./defaultValuesMy";
 
 import { darkpink } from "./colors";
 import { darkgreen } from "./colors";
@@ -36,5 +49,5 @@ export {
   green,
   darkblue,
   lightgreen,
-  lightpink
+  lightpink,
 };
